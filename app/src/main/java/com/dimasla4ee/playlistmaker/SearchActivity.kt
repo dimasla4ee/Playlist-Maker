eb244415f46494e317b9a4f2474e44a7b4dcf86f
de.krylov <de.krylov@vk.com>
2025-07-06 15:05:09 +0300

    feat(search): implement basic searchbar functionality
    - Added search bar logic in SearchActivity.kt with TextWatcher for dynamic clear button visibility
    - Implemented clear button click handler in SearchActivity.kt to reset search input and hide keyboard
    - Added search bar layout click handler in SearchActivity.kt to focus EditText

--- app/src/main/java/com/dimasla4ee/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/dimasla4ee/playlistmaker/SearchActivity.kt
@@ -1,11 +1,63 @@
 package com.dimasla4ee.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+
+        val searchBarLayout = findViewById<LinearLayout>(R.id.searchBar)
+        val searchBar = findViewById<EditText>(R.id.searchBarEditText)
+        val clearQueryButton = findViewById<ImageView>(R.id.clearSearchBarButton)
+
+        val searchBarTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                //TODO: Not yet implemented
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                clearQueryButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //TODO: Not yet implemented
+            }
+        }
+
+        searchBar.addTextChangedListener(searchBarTextWatcher)
+
+        clearQueryButton.setOnClickListener {
+            inputMethodManager?.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+            searchBar.apply {
+                setText("")
+                clearFocus()
+            }
+        }
+
+        searchBarLayout.setOnClickListener {
+            searchBar.requestFocus()
+        }
     }
+
 }
