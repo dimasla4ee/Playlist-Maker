c50cb52875519c7dfe54cdd4bcfe84a3749db725
de.krylov <de.krylov@vk.com>
2025-07-07 23:37:54 +0300

    feat(search): add search query persistence and back button navigation
    - Implemented onSaveInstanceState and onRestoreInstanceState in SearchActivity.kt to save and restore search query
    - Added back button click handler in SearchActivity.kt to close the activity

--- app/src/main/java/com/dimasla4ee/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/dimasla4ee/playlistmaker/SearchActivity.kt
@@ -6,20 +6,36 @@ import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    private var query: String = EMPTY_QUERY
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(QUERY_KEY, query)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        query = savedInstanceState.getString(QUERY_KEY, EMPTY_QUERY)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
 
-        val searchBarLayout = findViewById<LinearLayout>(R.id.searchBar)
-        val searchBar = findViewById<EditText>(R.id.searchBarEditText)
+        val searchbarLayout = findViewById<LinearLayout>(R.id.searchBar)
+        val searchbar = findViewById<EditText>(R.id.searchBarEditText)
         val clearQueryButton = findViewById<ImageView>(R.id.clearSearchBarButton)
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+
+        searchbar.setText(query)
 
         val searchBarTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(
@@ -38,6 +54,7 @@ class SearchActivity : AppCompatActivity() {
                 count: Int
             ) {
                 clearQueryButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                query = s?.toString() ?: EMPTY_QUERY
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -45,19 +62,27 @@ class SearchActivity : AppCompatActivity() {
             }
         }
 
-        searchBar.addTextChangedListener(searchBarTextWatcher)
+        searchbar.addTextChangedListener(searchBarTextWatcher)
 
         clearQueryButton.setOnClickListener {
             inputMethodManager?.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
-            searchBar.apply {
-                setText("")
+            searchbar.apply {
+                setText(EMPTY_QUERY)
                 clearFocus()
             }
         }
 
-        searchBarLayout.setOnClickListener {
-            searchBar.requestFocus()
+        searchbarLayout.setOnClickListener {
+            searchbar.requestFocus()
+        }
+
+        backButton.setOnClickListener {
+            finish()
         }
     }
 
+    companion object {
+        const val EMPTY_QUERY = ""
+        const val QUERY_KEY = "QUERY_KEY"
+    }
 }
