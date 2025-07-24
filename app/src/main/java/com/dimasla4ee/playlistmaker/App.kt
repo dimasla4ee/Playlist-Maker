2a03f76f44b830d7df95aed5695308e267314af2
de.krylov <de.krylov@vk.com>
2025-07-24 18:58:36 +0300

    fix(theme): Theme preferences now save properly

--- app/src/main/java/com/dimasla4ee/playlistmaker/App.kt
+++ app/src/main/java/com/dimasla4ee/playlistmaker/App.kt
@@ -1,30 +1,33 @@
 package com.dimasla4ee.playlistmaker
 
 import android.app.Application
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.core.content.edit
 
 class App : Application() {
 
     var darkTheme = false
+        private set
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate() {
         super.onCreate()
-
-        val sharedPreferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE)
         darkTheme = sharedPreferences.getBoolean(THEME_KEY, darkTheme)
+        switchTheme(darkTheme)
     }
 
     fun switchTheme(darkThemeEnabled: Boolean) {
         darkTheme = darkThemeEnabled
         AppCompatDelegate.setDefaultNightMode(
-            if (darkThemeEnabled) {
-                MODE_NIGHT_YES
-            } else {
-                MODE_NIGHT_NO
-            }
+            if (darkThemeEnabled) MODE_NIGHT_YES else MODE_NIGHT_NO
         )
+        sharedPreferences.edit {
+            putBoolean(THEME_KEY, darkThemeEnabled)
+        }
     }
 
     private companion object {
