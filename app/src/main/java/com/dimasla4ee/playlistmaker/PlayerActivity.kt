474b15fdb36b4315aac0ed9a0cc1f0b45ae34650
de.krylov <de.krylov@vk.com>
2025-07-28 13:16:40 +0300

    feat(player): implement player layout business logic and track integration
    - Added business logic to display track metadata in player activity
    - Enhanced search activity to launch player with selected track data
    - Extended track data class with metadata fields like album and genre
    - Updated player layout with specific icons and text styling
    - Moved ellipsize from track item to global text style
    - Added icons for play, playlist, and favorite actions
    - Refined button and theme styles for consistent UI appearance

--- app/src/main/java/com/dimasla4ee/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/dimasla4ee/playlistmaker/PlayerActivity.kt
@@ -1,35 +1,68 @@
 package com.dimasla4ee.playlistmaker
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.dimasla4ee.playlistmaker.databinding.ActivityPlayerBinding
+import com.google.gson.Gson
 
 class PlayerActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityPlayerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_player)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setupWindowInsets(binding.root)
+
+        val json = intent.getStringExtra("song_info")
+        val track = Gson().fromJson(json, Track::class.java)
+
+        binding.headerBackButton.setOnClickListener {
+            finish()
         }
+
+        //TODO: Implement snippets
+        binding.songCurrentDuration.text = "0:30"
+
+        binding.songDurationFetched.text = track.formatedDuration
+        track.year.let { year ->
+            if (year == null) {
+                binding.songYearFetched.visibility = View.GONE
+                binding.songYearLabel.visibility = View.GONE
+            } else {
+                binding.songYearFetched.text = year.toString()
+            }
+        }
+        track.album.let { album ->
+            if (album.isEmpty()) {
+                binding.songAlbumFetched.visibility = View.GONE
+                binding.songAlbumLabel.visibility = View.GONE
+            } else {
+                binding.songAlbumFetched.text = album
+            }
+        }
+        binding.songGenreFetched.text = track.genre
+        binding.songCountryFetched.text = track.country
+
+        binding.songTitle.text = track.title
+        binding.songAuthor.text = track.artist
 
         val radius = IMAGE_CORNER_RADIUS.dpToPx(this).toInt()
         val placeholder = AppCompatResources.getDrawable(this, R.drawable.ic_placeholder_45)
 
         placeholder?.setTint(getColor(R.color.light_gray))
 
-        val binding: ActivityPlayerBinding = ActivityPlayerBinding.inflate(layoutInflater)
-
         Glide.with(binding.root)
-            .load("https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
+            .load(track.urlBigArtwork)
             .placeholder(placeholder)
             .transform(RoundedCorners(radius))
             .fitCenter()
--- app/src/main/java/com/dimasla4ee/playlistmaker/Track.kt
+++ app/src/main/java/com/dimasla4ee/playlistmaker/Track.kt
@@ -8,9 +8,23 @@ data class Track(
     @SerializedName("trackId") val id: Int,
     @SerializedName("trackName") val title: String,
     @SerializedName("artistName") val artist: String,
+    @SerializedName("country") val country: String,
+    @SerializedName("collectionName") val album: String,
+    @SerializedName("releaseDate") val releaseDate: String,
+    @SerializedName("primaryGenreName") val genre: String,
     @SerializedName("trackTimeMillis") val durationInMillis: Long,
-    @SerializedName("artworkUrl100") val artworkUrl: String
+    @SerializedName("artworkUrl100") val urlPreviewArtwork: String
 ) {
+    val year: Int?
+        get() = releaseDate.substring(0, 4).toIntOrNull()
+
     val formatedDuration: String
         get() = SimpleDateFormat("mm:ss", Locale.getDefault()).format(durationInMillis)
+
+    val urlBigArtwork: String
+        get() = urlPreviewArtwork.replaceAfterLast('/', ARTWORK_BIG_RESOLUTION_SUFFIX)
+
+    private companion object {
+        const val ARTWORK_BIG_RESOLUTION_SUFFIX = "512x512bb.jpg"
+    }
 }
