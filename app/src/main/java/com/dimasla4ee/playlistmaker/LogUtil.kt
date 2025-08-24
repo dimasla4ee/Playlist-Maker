b7d84499ee716d1bc352c7d92a6f692311be8c23
de.krylov <de.krylov@vk.com>
2025-08-24 15:43:06 +0300

    feat(log): add error logging function to LogUtil
    - Added an `e` function to LogUtil.kt to log error messages.
    - This function, similar to the existing `d` function, will only output logs when `BuildConfig.DEBUG` is true.

--- app/src/main/java/com/dimasla4ee/playlistmaker/LogUtil.kt
+++ app/src/main/java/com/dimasla4ee/playlistmaker/LogUtil.kt
@@ -5,7 +5,7 @@ import android.util.Log
 /**
  * Utility object for logging messages.
  *
- * This object is a wrapper for Log API that provides methods to log debug messages
+ * This object is a wrapper for Log API that provides methods to log messages
  * that will only be output when the application is in debug mode(i.e., `BuildConfig.DEBUG` is true).
  */
 @Suppress("unused")
@@ -13,6 +13,12 @@ object LogUtil {
     fun d(tag: String, msg: String) {
         if (BuildConfig.DEBUG) {
             Log.d(tag, msg)
+        }
+    }
+
+    fun e(tag: String, msg: String) {
+        if (BuildConfig.DEBUG) {
+            Log.e(tag, msg)
         }
     }
 }
