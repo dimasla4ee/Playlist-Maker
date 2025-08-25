5d92a3a32bc286ac599c7f6f1cf6ec9e7618a461
de.krylov <de.krylov@vk.com>
2025-08-25 09:27:14 +0300

    feat(search): implement search request debounce
    - Added a generic `debounce` function to the `Debouncer` object.
    - This function takes a `Runnable` and uses the `DEFAULT_DEBOUNCE_MILLIS` delay.
    - The previous `TYPE_DEBOUNCE_MILLIS` constant was renamed to `DEFAULT_DEBOUNCE_MILLIS`.

--- app/src/main/java/com/dimasla4ee/playlistmaker/Debouncer.kt
+++ app/src/main/java/com/dimasla4ee/playlistmaker/Debouncer.kt
@@ -5,7 +5,7 @@ import android.os.Looper
 
 object Debouncer {
     private const val CLICK_DEBOUNCE_MILLIS = 1000L
-    private const val TYPE_DEBOUNCE_MILLIS = 2000L
+    private const val DEFAULT_DEBOUNCE_MILLIS = 2000L
 
     private var isClickAllowed = true
 
@@ -18,5 +18,12 @@ object Debouncer {
             handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_MILLIS)
         }
         return current
+    }
+
+    fun debounce(
+        action: Runnable
+    ) {
+        handler.removeCallbacks(action)
+        handler.postDelayed(action, DEFAULT_DEBOUNCE_MILLIS)
     }
 }
