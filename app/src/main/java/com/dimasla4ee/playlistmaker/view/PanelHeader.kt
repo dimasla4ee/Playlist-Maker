d439ab85bd9787af9fd958bae0a5cec1b22ac713
de.krylov <de.krylov@vk.com>
2025-08-27 19:07:46 +0300

    refactor(view): update PanelHeader to use ViewBinding
    - Renamed view IDs in `view_panel_header.xml` for clarity:
        - `headerBackButton` to `iconButton`
        - `headerTitleTextView` to `titleTextView`
    - Migrated `PanelHeader.kt` to use ViewBinding:
        - Inflated layout using `ViewPanelHeaderBinding`.
        - Accessed views through the `binding` object.
    - Introduced `text` and `isIconVisible` properties with getters and setters for easier view manipulation.
    - Updated attribute handling to use the `binding` object.

--- app/src/main/java/com/dimasla4ee/playlistmaker/view/PanelHeader.kt
+++ app/src/main/java/com/dimasla4ee/playlistmaker/view/PanelHeader.kt
@@ -2,10 +2,11 @@ package com.dimasla4ee.playlistmaker.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.ImageButton
+import android.view.LayoutInflater
 import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.core.view.isVisible
 import com.dimasla4ee.playlistmaker.R
+import com.dimasla4ee.playlistmaker.databinding.ViewPanelHeaderBinding
 
 @Suppress("unused")
 class PanelHeader @JvmOverloads constructor(
@@ -14,52 +15,64 @@ class PanelHeader @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
-    private val iconButton: ImageButton
-    private val titleView: TextView
+    private val binding: ViewPanelHeaderBinding
 
     init {
         orientation = HORIZONTAL
-        inflate(context, R.layout.view_panel_header, this)
-
-        iconButton = findViewById(R.id.headerBackButton)
-        titleView = findViewById(R.id.headerTitleTextView)
+        binding = ViewPanelHeaderBinding.inflate(LayoutInflater.from(context), this)
 
         context.theme.obtainStyledAttributes(
             attrs, R.styleable.PanelHeader, defStyleAttr, 0
         ).apply {
             try {
-                titleView.text = getString(R.styleable.PanelHeader_android_text)
-                titleView.setTextColor(
-                    getColor(R.styleable.PanelHeader_android_textColor, titleView.currentTextColor)
-                )
-                if (hasValue(R.styleable.PanelHeader_android_textSize)) {
-                    titleView.textSize = getDimension(
-                        R.styleable.PanelHeader_android_textSize, titleView.textSize
+                binding.titleTextView.apply {
+                    text = getString(R.styleable.PanelHeader_android_text)
+
+                    setTextColor(
+                        getColor(
+                            R.styleable.PanelHeader_android_textColor, currentTextColor
+                        )
                     )
+
+                    if (hasValue(R.styleable.PanelHeader_android_textSize)) {
+                        textSize = getDimension(
+                            R.styleable.PanelHeader_android_textSize, textSize
+                        )
+                    }
                 }
-                val showIcon = getBoolean(R.styleable.PanelHeader_showIcon, false)
-                iconButton.visibility = if (showIcon) VISIBLE else GONE
-                if (hasValue(R.styleable.PanelHeader_iconSrc)) {
-                    iconButton.setImageResource(getResourceId(R.styleable.PanelHeader_iconSrc, 0))
+
+                binding.iconButton.apply {
+                    val showIcon = getBoolean(R.styleable.PanelHeader_showIcon, false)
+                    isIconVisible = showIcon
+
+                    if (hasValue(R.styleable.PanelHeader_iconSrc)) {
+                        setImageResource(getResourceId(R.styleable.PanelHeader_iconSrc, 0))
+                    }
+
+                    if (hasValue(R.styleable.PanelHeader_iconTint)) {
+                        imageTintList = getColorStateList(R.styleable.PanelHeader_iconTint)
+                    }
                 }
-                if (hasValue(R.styleable.PanelHeader_iconTint)) {
-                    iconButton.imageTintList = getColorStateList(R.styleable.PanelHeader_iconTint)
-                }
+
             } finally {
                 recycle()
             }
         }
     }
 
-    fun setText(text: CharSequence) {
-        titleView.text = text
-    }
+    var text: CharSequence
+        get() = binding.titleTextView.text
+        set(value) {
+            binding.titleTextView.text = text
+        }
 
     fun setOnIconClickListener(listener: OnClickListener) {
-        iconButton.setOnClickListener(listener)
+        binding.iconButton.setOnClickListener(listener)
     }
 
-    fun setIconVisible(visible: Boolean) {
-        iconButton.visibility = if (visible) VISIBLE else GONE
-    }
+    var isIconVisible: Boolean
+        get() = binding.iconButton.isVisible
+        set(value) {
+            binding.iconButton.visibility = if (value) VISIBLE else GONE
+        }
 }
