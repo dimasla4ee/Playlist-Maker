7825cd80dd0c79fd7831b2997a516c0926b1ce46
de.krylov <de.krylov@vk.com>
2025-08-29 09:52:48 +0300

    refactor(search): enhance Debouncer functionality
    - Added a `delayMillis` parameter to the `debounce` function for customizable debounce intervals.
    - Refactored `isClickAllowed` to use the updated `debounce` function with `CLICK_DEBOUNCE_MILLIS`.
    - Made `CLICK_DEBOUNCE_MILLIS` and `DEFAULT_DEBOUNCE_MILLIS` public constants.

--- app/src/main/java/com/dimasla4ee/playlistmaker/Debouncer.kt
+++ app/src/main/java/com/dimasla4ee/playlistmaker/Debouncer.kt
@@ -4,26 +4,27 @@ import android.os.Handler
 import android.os.Looper
 
 object Debouncer {
-    private const val CLICK_DEBOUNCE_MILLIS = 1000L
-    private const val DEFAULT_DEBOUNCE_MILLIS = 2000L
+    const val CLICK_DEBOUNCE_MILLIS = 1000L
+    const val DEFAULT_DEBOUNCE_MILLIS = 2000L
 
     private var isClickAllowed = true
 
     private val handler = Handler(Looper.getMainLooper())
 
+    fun debounce(
+        delayMillis: Long = DEFAULT_DEBOUNCE_MILLIS,
+        action: Runnable
+    ) {
+        handler.removeCallbacks(action)
+        handler.postDelayed(action, delayMillis)
+    }
+
     fun isClickAllowed(): Boolean {
         val current = isClickAllowed
         if (isClickAllowed) {
             isClickAllowed = false
-            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_MILLIS)
+            debounce(CLICK_DEBOUNCE_MILLIS) { isClickAllowed = true }
         }
         return current
-    }
-
-    fun debounce(
-        action: Runnable
-    ) {
-        handler.removeCallbacks(action)
-        handler.postDelayed(action, DEFAULT_DEBOUNCE_MILLIS)
     }
 }
